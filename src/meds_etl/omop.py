1b4d89e56de57df4173af407c503d813a44b5b5f
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@hpi.de>
2025-02-20 08:08:55 -0500

    standard concepts, then source

--- src/meds_etl/omop.py
+++ src/meds_etl/omop.py
@@ -209,6 +209,8 @@ def write_event_data(
             concept_id = (
                 pl.when(concept_id != 0)
                 .then(concept_id)
+                .when(source_concept_id != 0)
+                .then(source_concept_id)
                 .otherwise(fallback_concept_id)
             )
 
